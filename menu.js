09115fae1090dc603661bbb2c9e15fa30aa4b78b
Sparrow-Tech06 <cpal980075@gmail.com>
2026-03-01 18:44:52 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -46,7 +46,7 @@ document.addEventListener("DOMContentLoaded", () => {
   #menuOverlay{
     position:fixed;
     inset:0;
-    background:#7EACB5;
+    background:#fff;
     z-index:9999;
     display:flex;
     padding:16px;
